a654c99b26405df668cb6247f6440318059b2f44
Dan <danramosmarques123@gmail.com>
2025-09-29 13:49:13 -0300

    Create Funcao_soma.py

---
+++ Funcao_soma.py
@@ -0,0 +1,12 @@
+def soma(numero1,numero2,numero3):
+    return numero1 + numero2 + numero3
+
+if __name__ == '__main__':
+    valor1 = int(input("Digite o valor 1: "))
+    valor2 = int(input("Digite o valor 2: "))
+    valor3 = int(input("Digite o valor 3: "))
+
+    def main():
+        print(soma(valor1,valor2,valor3))
+
+main()
